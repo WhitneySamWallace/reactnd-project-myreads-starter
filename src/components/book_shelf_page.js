161707ae7e358f6dc014208f06cc77db52f7f4bf
Whitney Sam <whitneysam@Whitneys-MacBook-Pro.local>
2018-08-07 13:18:24 -0700

    added API call

--- src/components/book_shelf_page.js
+++ src/components/book_shelf_page.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Bookshelf from './book_shelf';
+import * as BooksAPI from '../BooksAPI';
 
 class BookShelfPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: []
+        };
+
+        BooksAPI.getAll()
+        .then(booksFromApi => {
+            this.setState({ books: booksFromApi })
+        });
+    }
+
     render() {
         return (
         <div className="list-books">
@@ -11,9 +25,9 @@ class BookShelfPage extends Component {
             </div>
             <div className="list-books-content">
               <div>
-                <Bookshelf />
-                <Bookshelf />
-                <Bookshelf />
+                <Bookshelf title="Currently Reading" books={this.state.books.filter(book => book.shelf == 'currentlyReading')} />
+                <Bookshelf title="Want to Read" books={this.state.books.filter(book => book.shelf == 'wantToRead')} />
+                <Bookshelf title="Read" books={this.state.books.filter(book => book.shelf == 'read')} />
               </div>
             </div>
             <div className="open-search">
